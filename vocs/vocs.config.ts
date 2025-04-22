b918bf716f4e57484e71377242a6a6fe2c8d3d5c
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-04-22 19:43:38 +0530

    init vocs

---
+++ vocs/vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Docs',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'Example',
+      link: '/example',
+    },
+  ],
+})
