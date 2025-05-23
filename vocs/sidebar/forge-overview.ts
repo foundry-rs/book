e57d72143736a6b136f7a58f496dae91fec04328
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-05-23 16:14:06 +0100

    fix forge links in sidebar

--- vocs/sidebar/forge-overview.ts
+++ vocs/sidebar/forge-overview.ts
@@ -9,21 +9,21 @@ export const forgeOverview: SidebarItem[] = [
         text: 'Tests',
         link: '/forge/tests',
         items: [
-            { text: 'Writing Tests', link: '/forge/writing-tests' },
-            { text: 'Cheatcodes', link: '/forge/cheatcodes' },
-            { text: 'Forge Standard Library Overview', link: '/forge/forge-std' },
-            { text: 'Understanding Traces', link: '/forge/traces' },
-            { text: 'Fork Testing', link: '/forge/fork-testing' },
-            { text: 'Replaying Failures', link: '/forge/replay-testing' }
+            { text: 'Writing Tests', link: '/forge/tests/writing-tests' },
+            { text: 'Cheatcodes', link: '/forge/tests/cheatcodes' },
+            { text: 'Forge Standard Library Overview', link: '/forge/tests/forge-std' },
+            { text: 'Understanding Traces', link: '/forge/tests/traces' },
+            { text: 'Fork Testing', link: '/forge/tests/fork-testing' },
+            { text: 'Replaying Failures', link: '/forge/tests/replay-testing' }
         ]
     },
     {
         text: 'Advanced Testing',
         link: '/forge/advanced-testing',
         items: [
-            { text: 'Fuzz Testing', link: '/forge/fuzz-testing' },
-            { text: 'Invariant Testing', link: '/forge/invariant-testing' },
-            { text: 'Differential Testing', link: '/forge/differential-ffi-testing' }
+            { text: 'Fuzz Testing', link: '/forge/advanced-testing/fuzz-testing' },
+            { text: 'Invariant Testing', link: '/forge/advanced-testing/invariant-testing' },
+            { text: 'Differential Testing', link: '/forge/advanced-testing/differential-ffi-testing' }
         ]
     },
     { text: 'Deploying and Verifying', link: '/forge/deploying' },
@@ -31,9 +31,9 @@ export const forgeOverview: SidebarItem[] = [
         text: 'Gas Tracking',
         link: '/forge/gas-tracking',
         items: [
-            { text: 'Gas Reports', link: '/forge/gas-reports' },
-            { text: 'Gas Function Snapshots', link: '/forge/gas-function-snapshots' },
-            { text: 'Gas Section Snapshots', link: '/forge/gas-section-snapshots' }
+            { text: 'Gas Reports', link: '/forge/gas-tracking/gas-reports' },
+            { text: 'Gas Function Snapshots', link: '/forge/gas-tracking/gas-function-snapshots' },
+            { text: 'Gas Section Snapshots', link: '/forge/gas-tracking/gas-section-snapshots' }
         ]
     },
     { text: 'Debugger', link: '/forge/debugger' }
