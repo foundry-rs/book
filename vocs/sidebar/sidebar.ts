2b3bc50137de10ffd4b5152aa8100c2bc11d757d
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-07-24 21:01:24 +0530

    feat: benchmarks page (#1611)
    feat: benchmarks

--- vocs/sidebar/sidebar.ts
+++ vocs/sidebar/sidebar.ts
@@ -13,6 +13,7 @@ export const sidebar: Sidebar = [
       { text: "Installation", link: "/introduction/installation" },
       { text: "Getting Started", link: "/introduction/getting-started" },
       { text: "Prompting", link: "/introduction/prompting" },
+      { text: "Benchmarks", link: "/introduction/benchmarks" },
     ],
   },
   {
