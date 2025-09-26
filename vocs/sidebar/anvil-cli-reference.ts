50df3cba9128ece16dea3f2423d79b376bafa146
Oliver Nordbjerg <hi@notbjerg.me>
2025-09-26 17:08:49 +0200

    docs: remove fig

--- vocs/sidebar/anvil-cli-reference.ts
+++ vocs/sidebar/anvil-cli-reference.ts
@@ -1,17 +1,16 @@
 import { SidebarItem } from "vocs";
 
 export const anvilCliReference: SidebarItem = {
-    text: "Reference",
-    collapsed: true,
-    items: [
-        { text: "anvil", link: "/anvil/reference/anvil" },
-        {
-            text: "General Commands",
-            collapsed: true,
-            items: [
-                { text: "anvil completions", link: "/anvil/reference/completions" },
-                { text: "anvil generate-fig-spec", link: "/anvil/reference/generate-fig-spec" },
-            ],
-        },
-    ],
+  text: "Reference",
+  collapsed: true,
+  items: [
+    { text: "anvil", link: "/anvil/reference/anvil" },
+    {
+      text: "General Commands",
+      collapsed: true,
+      items: [
+        { text: "anvil completions", link: "/anvil/reference/completions" },
+      ],
+    },
+  ],
 };
