f0e456b78fe102ec70c56a935eb14bcc6b869bd4
mattsse <19890894+mattsse@users.noreply.github.com>
2025-10-05 00:28:07 +0000

    update command output

--- vocs/sidebar/anvil-cli-reference.ts
+++ vocs/sidebar/anvil-cli-reference.ts
@@ -1,16 +1,16 @@
 import { SidebarItem } from "vocs";
 
 export const anvilCliReference: SidebarItem = {
-  text: "Reference",
-  collapsed: true,
-  items: [
-    { text: "anvil", link: "/anvil/reference/anvil" },
-    {
-      text: "General Commands",
-      collapsed: true,
-      items: [
-        { text: "anvil completions", link: "/anvil/reference/completions" },
-      ],
-    },
-  ],
+    text: "Reference",
+    collapsed: true,
+    items: [
+        { text: "anvil", link: "/anvil/reference/anvil" },
+        {
+            text: "General Commands",
+            collapsed: true,
+            items: [
+                { text: "anvil completions", link: "/anvil/reference/completions" },
+            ],
+        },
+    ],
 };
