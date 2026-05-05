4f7ac3a40076ff86e1dc6cdbfff014db2daa1f0b
Mablr <59505383+mablr@users.noreply.github.com>
2026-05-05 15:35:31 +0200

    fix(lint): add tx-origin lint to sidebar and main page

--- sidebar/sidebar.ts
+++ sidebar/sidebar.ts
@@ -58,6 +58,7 @@ const docs = [
               { text: "divide-before-multiply", link: "/forge/linting/divide-before-multiply" },
               { text: "incorrect-erc20-interface", link: "/forge/linting/incorrect-erc20-interface" },
               { text: "incorrect-erc721-interface", link: "/forge/linting/incorrect-erc721-interface" },
+              { text: "tx-origin", link: "/forge/linting/tx-origin" },
               { text: "unsafe-typecast", link: "/forge/linting/unsafe-typecast" },
             ],
           },
